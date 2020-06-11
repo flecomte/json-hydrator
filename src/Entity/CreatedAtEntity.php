bd8a615e721035346e3641a6aa58899e670889a9
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2020-06-11 18:15:43 +0200

    Add serializer annotation for date

--- src/Entity/CreatedAtEntity.php
+++ src/Entity/CreatedAtEntity.php
@@ -3,9 +3,13 @@
 namespace FLE\JsonHydrator\Entity;
 
 use DateTime;
+use JMS\Serializer\Annotation as Serializer;
 
 trait CreatedAtEntity
 {
+    /**
+     * @Serializer\Type("DateTime<'Y-m-d\TH:i:s.u'>")
+     */
     protected DateTime $createdAt;
 
     public function __construct()
--- src/Entity/UpdatedAtEntity.php
+++ src/Entity/UpdatedAtEntity.php
@@ -3,9 +3,13 @@
 namespace FLE\JsonHydrator\Entity;
 
 use DateTime;
+use JMS\Serializer\Annotation as Serializer;
 
 trait UpdatedAtEntity
 {
+    /**
+     * @Serializer\Type("DateTime<'Y-m-d\TH:i:s.u'>")
+     */
     protected DateTime $updatedAt;
 
     public function getUpdatedAt(): ?DateTime
