652877a9015e3f58f797f8a069d14487bd817b14
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2020-06-19 11:53:44 +0200

    Fix multiple migration file

--- src/Migration/sql/request/findAll.sql
+++ src/Migration/sql/request/findAll.sql
@@ -1,4 +1,7 @@
-SELECT json_object_agg(filename, h)
-FROM migration.history h
-GROUP BY h.filename
-ORDER BY h.filename DESC;
+SELECT json_object_agg(m.filename, m) as resource
+FROM (
+    SELECT h.*
+    FROM migration.history h
+    GROUP BY h.filename
+    ORDER BY h.filename DESC
+) m;
